082f2646ab827b7341a3fb69d04862b39266ccae
hao lre <ovaoval1n@haodeMacBook-Air.local>
2025-04-17 18:34:28 +0800

    hw09-marco

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -1,18 +1,26 @@
 (define (curry-cook formals body) 
 (if (null? formals)
-body  ; 如果没有参数了，返回 body
-(let ((param (car formals)))  ; 获取第一个参数
-  (list 'lambda (list param)  ; 返回一个新的 lambda
-        (curry-cook (cdr formals) body)))))  ; 递归处理剩余的参数)
+body  ; if no body,return body
+(let ((param (car formals)))  ; car first formals
+  (list 'lambda (list param)  ; new lambda
+        (curry-cook (cdr formals) body)))))  
+        ; Recursively process the remaining parameters
 
-(define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+(define (curry-consume curry args);get args,return lambda
+(if (null? args)  
+curry                  
+(curry-consume         
+    (curry (car args))  
+    (cdr args)          
+)))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
 
+(define (cadr lst) (car (cdr lst)))
+
 (define (switch-to-cond switch-expr)
-  (cons _________
+  (cons 'cond
         (map (lambda (option)
-               (cons _______________ (cdr option)))
+               (cons (list 'equal? (cadr switch-expr) (car option))(cdr option)))
              (car (cdr (cdr switch-expr))))))
